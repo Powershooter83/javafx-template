a76bcf68c4c5558d5f35bcb533fbe24e83cfef49
Yanick <yanick.romere@outlook.de>
2024-02-16 23:43:45 +0100

    update

--- src/main/java/me/prouge/template/service/LoginService.java
+++ src/main/java/me/prouge/template/service/LoginService.java
@@ -1,31 +1,30 @@
 package me.prouge.template.service;
 
 import com.google.inject.Inject;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.TypedQuery;
+import me.prouge.guicefx.persistence.DatabaseManager;
 import me.prouge.template.entities.UserEntity;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.List;
+
 public class LoginService {
 
     @Inject
-    private EntityManager entityManager;
+    private DatabaseManager databaseManager;
     private static final Logger logger = LogManager.getLogger();
 
     public boolean login(final String email, final String pwd) {
-        TypedQuery<UserEntity> typedQuery
-                = entityManager.createQuery("SELECT u FROM UserEntity u WHERE u.email=:email AND u.password=:pwd", UserEntity.class);
-        typedQuery.setParameter("email", email);
-        typedQuery.setParameter("pwd", pwd);
+        List<UserEntity> users = databaseManager.executeQueryWithResultList("SELECT u FROM UserEntity u WHERE u.email=:email AND u.password=:pwd", UserEntity.class, q -> {
+            q.setParameter("email", email);
+            q.setParameter("pwd", pwd);
+        });
 
-        return typedQuery.getResultList().size() == 1;
+        return users.size() == 1;
     }
 
     public void register(final String email, final String pwd) {
-        entityManager.getTransaction().begin();
-        entityManager.persist(new UserEntity(email, pwd));
-
+        databaseManager.runInTransaction(e -> e.persist(new UserEntity(email, pwd)));
         // logger.error("FEHLER");
     }
 
